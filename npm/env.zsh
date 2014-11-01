0bfe5b6d3a5e1e2d669f61e4bb02fff875fcbcc3
Dave Newman <dave@snappyco.de>
2014-11-01 13:01:53 -0700

    Add some stuff

--- npm/env.zsh
+++ npm/env.zsh
@@ -1,1 +1,2 @@
 export PATH=$PATH:/usr/local/share/npm/bin
+alias node='node --harmony'
--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$HOME/code/go/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$HOME/code/go/bin:$PATH"
 export PATH="/Applications/Postgres.app/Contents/Versions/9.3/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
